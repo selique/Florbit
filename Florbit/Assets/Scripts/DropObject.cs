fb15378a3c859329fbcd2322ca4fabc9075d0563
Selique Moisés lopes ferreira <Selique Moiss lopes ferreira>
2016-03-10 05:15:29 -0300

    fix path

---
+++ Florbit/Assets/Scripts/DropObject.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class DropObject : MonoBehaviour, IDropHandler, IPointerEnterHandler, IPointerExitHandler
+{
+    public Image iconImage;
+    private Sprite nowSprite;
+
+    void Start()
+    {
+        nowSprite = null;
+    }
+
+    public void OnPointerEnter(PointerEventData pointerEventData)
+    {
+        if(pointerEventData.pointerDrag == null) return;
+        Image droppedImage = pointerEventData.pointerDrag.GetComponent<Image>();
+        iconImage.sprite = droppedImage.sprite;
+        iconImage.color = Vector4.one * 0.6f;
+    }
+
+    public void OnPointerExit(PointerEventData pointerEventData)
+    {
+        if(pointerEventData.pointerDrag == null) return;
+        iconImage.sprite = nowSprite;
+        if(nowSprite == null)
+            iconImage.color = Vector4.zero;
+        else
+            iconImage.color = Vector4.one;
+    }
+    public void OnDrop(PointerEventData pointerEventData)
+    {
+        Image droppedImage = pointerEventData.pointerDrag.GetComponent<Image>();
+        iconImage.sprite = droppedImage.sprite;
+        nowSprite = droppedImage.sprite;
+        iconImage.color = Vector4.one;
+    }
+}
