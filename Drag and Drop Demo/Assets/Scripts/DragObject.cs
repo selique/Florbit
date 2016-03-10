1f3b70c9d95bdca9273ae23b8107189c83cdb5f0
Selique Moisés lopes ferreira <Selique Moiss lopes ferreira>
2016-03-10 05:11:38 -0300

    prototype Hack drag and drop

---
+++ Drag and Drop Demo/Assets/Scripts/DragObject.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+[RequireComponent(typeof(Image))]
+public class DragObject : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    void IBeginDragHandler.OnBeginDrag(PointerEventData pointerEventData)
+    {
+        Debug.Log("start DragObject OnBeginDrag");
+    }
+    public void OnDrag(PointerEventData pointerEventData)
+    {
+        Debug.Log("start DragObject OnDrag");
+    }
+    public void OnEndDrag(PointerEventData pointerEventData)
+    {
+        Debug.Log("start DragObject OnEndDrag");
+    }
+}
---
+++ Drag and Drop Demo/Assets/Scripts/DropObject.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class DropObject : MonoBehaviour, IDropHandler, IPointerEnterHandler, IPointerExitHandler
+{
+    public void OnPointerEnter(PointerEventData pointerEventData)
+    {
+        Debug.Log("start DropObject OnPointerEnter");
+    }
+    public void OnPointerExit(PointerEventData pointerEventData)
+    {
+        Debug.Log("start DropObject OnPointerExit");
+    }
+    public void OnDrop(PointerEventData pointerEventData)
+    {
+        Debug.Log("start DropObject OnDrop");
+    }
+}
